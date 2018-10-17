696a43d6511fe82d4313dfbf31a787e33dd51d37
Ester Pratt <esterpratt@gmail.com>
2018-10-17 16:13:06 +0300

    3d commit

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@
 function toggleMenu() {
     document.querySelector('.main-nav').classList.toggle('open');
 }
+
+function toggleModal() {
+    document.querySelector('.modal-container').classList.toggle('open');
+}
